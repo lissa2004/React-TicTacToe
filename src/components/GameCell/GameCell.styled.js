9fb7aa0c5e22ea61720380a8ddb6326d1f7d5e89
[lissa2004] <[mathikgek21@gmail.com]>
2024-04-10 00:24:13 +0200

    fixed the game board bug

--- src/components/GameCell/GameCell.styled.js
+++ src/components/GameCell/GameCell.styled.js
@@ -3,14 +3,15 @@ import styled from "styled-components";
 export const CellStyle = styled.button`
   background-color: ${(props) => props.isWinningCell ? props.theme.colors.yellow : props.theme.colors.secondary};
   color: ${(props) => props.theme.colors.primary};
-  font-size: 3rem;
+  font-size: 6rem;
   border: none;
   width: 10rem;
-  height: 4rem;
+  height: 7rem;
   border-radius: 2.5rem;
-  box-shadow: 5px 10px ${(props) => props.theme.colors.cream};
+  box-shadow: 3px 6px ${(props) => props.theme.colors.cream};
   cursor: pointer;
-  padding: 4rem;
+  padding:2rem;
+  
   
 
   .markedItem {
@@ -22,6 +23,7 @@ export const CellStyle = styled.button`
     path {
       stroke: ${(props) => props.theme.colors.primary};
       stroke-width: 0;
+      stroke-height:70px;
     }
   }
 
